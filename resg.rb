c81c3be734f6d50db11c1313a78978083856892c
Jordan Robinson <hypahnova@gmail.com>
2015-04-19 17:26:56 +0100

    Update for logging and cron jobs

--- resg.rb
+++ resg.rb
@@ -16,45 +16,42 @@ mailgun_key = lines[3].split('=')[1]
 
 users = lines[4].split(',')
 
-while true
-  users.each { |user|
+users.each { |user|
 
-    uri = URI.parse('https://api.resourceguruapp.com/v1/buildingblocks/resources/' + user + '/')
-    req = Net::HTTP::Get.new(uri.path)
+  uri = URI.parse('https://api.resourceguruapp.com/v1/buildingblocks/resources/' + user + '/')
+  req = Net::HTTP::Get.new(uri.path)
 
-    req.basic_auth username, password
+  req.basic_auth username, password
 
-    res = Net::HTTP.start(uri.host,
-                          uri.port,
-                          :use_ssl => uri.scheme == 'https') { |http|
-      http.request(req)
-    }
-
-    parsed_json = JSON.parse(res.body)
-    name = parsed_json['name']
-    updated = parsed_json['updated_at'].gsub('T', ' ')
-    email = parsed_json['email']
-
-    filepath = user + '-lastupdated.inf'
-
-    if File.file?(filepath)
-      last_update = File.read(filepath)
-    else
-      File.write(filepath, updated)
-      last_update = updated
-    end
-
-    unless updated.eql?(last_update)
-      File.write(filepath, updated)
-      RestClient.post "https://api:key-" + mailgun_key + "@api.mailgun.net/v3/" + mailgun_acc + ".mailgun.org/messages",
-                      :from => "Resourceful <postmaster@" + mailgun_acc + ".mailgun.org>",
-                      :to => name + "<" + email + ">",
-                      :subject => "Resource Guru has updated",
-                      :text => name + ", Resource guru has changed!\nprevious:\t" + last_update + "\nnew:\t\t" + updated
-
-      puts 'sending email to ' + name + ' for ' + updated
-    end
+  res = Net::HTTP.start(uri.host,
+                        uri.port,
+                        :use_ssl => uri.scheme == 'https') { |http|
+    http.request(req)
   }
-  sleep 900
 
-end
+  parsed_json = JSON.parse(res.body)
+  name = parsed_json['name']
+  updated = parsed_json['updated_at'].gsub('T', ' ')
+  email = parsed_json['email']
+
+  filepath = user + '-lastupdated.inf'
+
+  if File.file?(filepath)
+    last_update = File.read(filepath)
+  else
+    File.write(filepath, updated)
+    last_update = updated
+  end
+
+  unless updated.eql?(last_update)
+    File.write(filepath, updated)
+    RestClient.post "https://api:key-" + mailgun_key + "@api.mailgun.net/v3/" + mailgun_acc + ".mailgun.org/messages",
+                    :from => "Resourceful <postmaster@" + mailgun_acc + ".mailgun.org>",
+                    :to => name + "<" + email + ">",
+                    :subject => "Resource Guru has updated",
+                    :text => name + ", Resource guru has changed!\nprevious:\t" + last_update + "\nnew:\t\t" + updated
+
+    puts 'sending email to ' + name + ' for ' + updated
+  end
+  puts 'ran at ' + Time.now.getutc.to_s + ' for ' + email
+}
