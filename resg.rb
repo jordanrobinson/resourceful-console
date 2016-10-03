31d2c9bc29dd144a7f89a5032531e6f1ea2710c8
Jordan Robinson <me@jordanrobinson.co.uk>
2016-10-03 23:33:02 +0100

    Cleaning up the main app + adding prefs.json

--- resg.rb
+++ resg.rb
@@ -5,31 +5,24 @@ require 'net/smtp'
 require 'json'
 require 'rubygems'
 require 'rest-client'
+require 'openssl'
 
-details = File.read('resourceguru.inf')
+prefs = JSON.parse(File.read('prefs.json'))
 
-lines = details.split("\n")
-username = lines[0].split('=')[1]
-password = lines[1].split('=')[1]
-mailgun_acc = lines[2].split('=')[1]
-mailgun_key = lines[3].split('=')[1]
+username = prefs['username']
+password = prefs['password']
+mailgun_acc = prefs['mailgun_acc']
+mailgun_key = prefs['mailgun_key']
 
-users = lines[4].split(',')
+users = prefs['accounts']
 
 users.each { |user|
 
-  uri = URI.parse('https://api.resourceguruapp.com/v1/buildingblocks/resources/' + user + '/')
-  req = Net::HTTP::Get.new(uri.path)
+  url = 'https://api.resourceguruapp.com/v1/buildingblocks/resources/' + user + '/'
+  res = RestClient::Request.execute method: :get, url: url, user: username, password: password, verify_ssl: false
 
-  req.basic_auth username, password
+  parsed_json = JSON.parse(res)
 
-  res = Net::HTTP.start(uri.host,
-                        uri.port,
-                        :use_ssl => uri.scheme == 'https') { |http|
-    http.request(req)
-  }
-
-  parsed_json = JSON.parse(res.body)
   name = parsed_json['name']
   updated = parsed_json['updated_at'].gsub('T', ' ')
   email = parsed_json['email']
@@ -45,11 +38,28 @@ users.each { |user|
 
   unless updated.eql?(last_update)
     File.write(filepath, updated)
-    RestClient.post "https://api:key-" + mailgun_key + "@api.mailgun.net/v3/" + mailgun_acc + ".mailgun.org/messages",
-                    :from => "Resourceful <postmaster@" + mailgun_acc + ".mailgun.org>",
-                    :to => name + "<" + email + ">",
-                    :subject => "Resource Guru has updated",
-                    :text => name + ", Resource guru has changed!\nprevious:\t" + last_update + "\nnew:\t\t" + updated
+
+    today = Time.now.strftime('%F')
+
+    url = 'https://api.resourceguruapp.com/v1/buildingblocks/reports/resources/' + user + '?start_date=' + today + '&end_date=2020-12-25'
+    res = RestClient::Request.execute method: :get, url: url, user: username, password: password, verify_ssl: false
+
+    parsed_json = JSON.parse(res)
+    projects = parsed_json['projects']
+
+    project_list = ''
+
+    projects.each { |project|
+      unless project['name'] == 'Bank Holiday' || project['name'] == 'Holiday'
+        project_list += project['name'] + "\n"
+      end
+    }
+
+    RestClient.post 'https://api:key-' + mailgun_key + '@api.mailgun.net/v3/' + mailgun_acc + '.mailgun.org/messages',
+                    :from => 'Resourceful <postmaster@' + mailgun_acc + '.mailgun.org>',
+                    :to => name + '<' + email + '>',
+                    :subject => 'Resource Guru has updated',
+                    :text => name + ", Resource guru has changed!\nprevious:\t" + last_update + "\nnew:\t\t" + updated + "\nCurrent projects:\n\n" + project_list
 
     puts 'sending email to ' + name + ' for ' + updated
   end
